9942327a1f3a0a266d3f772c69b0619f38b6035b
chole <claire@chloes-macbook.local>
2021-09-02 10:13:16 +0900

    Modify : CSS 속성 순서 적용

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,18 +1,1 @@
-import React, { Component } from 'react';
-import './Nav.scss';
 
-class Nav extends Component {
-  render() {
-    return (
-      <header className="nav">
-        <h1>
-          <a href="#">
-            <img alt="logo" />
-          </a>
-        </h1>
-      </header>
-    );
-  }
-}
-
-export default Nav;
