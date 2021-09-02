7cfb7e817b7b924762dfe1a138fc999f3f7d60ca
chole <claire@chloes-macbook.local>
2021-09-02 15:59:40 +0900

    Modify : 1차 코드리뷰 반영 완료

--- src/pages/List/ListCategoryBar.js
+++ src/pages/List/ListCategoryBar.js
@@ -10,7 +10,7 @@ class ListCategoryBar extends Component {
           className="listCategoryIcon"
           src="https://image.istarbucks.co.kr/common/img/menu/logo_decaf.png"
           alt="beverageCategoryIcon"
-        ></img>
+        />
         <p className="listCategoryExplain">
           디카페인 에스프레소 샷 추가 가능 (일부 음료 제외)
         </p>
