5042b449cc692d9e21517f3d3f6ef164c9e9b8a3
songmiO <dhthd1060@gmail.com>
2022-09-19 21:06:56 +0900

    Remove : Remove console

--- src/pages/Login/LoginTitle.js
+++ src/pages/Login/LoginTitle.js
@@ -3,7 +3,6 @@ import './LoginTitle.scss';
 
 class LoginTitle extends Component {
   render() {
-    console.log('확인');
     return (
       <div className="loginTitle">
         <div className="title">Ssongbucks</div>
