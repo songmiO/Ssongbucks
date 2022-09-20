6f4cee8a2a995defc985df2d3c336965c3b8b342
songmiO <dhthd1060@gmail.com>
2022-09-20 22:09:59 +0900

    Add : Add console

--- src/pages/Login/LoginTitle.js
+++ src/pages/Login/LoginTitle.js
@@ -3,6 +3,7 @@ import './LoginTitle.scss';
 
 class LoginTitle extends Component {
   render() {
+    console.log('확인');
     return (
       <div className="loginTitle">
         <div className="title">Ssongbucks</div>
